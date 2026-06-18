6c63fe97f525574bd76c33e22610541c325a2abd
Patryk Pochopin <280149@student.pwr.edu.pl>
2026-06-18 11:08:11 +0200

    Dodanie UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -6,12 +8,42 @@ public class PlayerHealth : MonoBehaviour
     public int healths = 3;
     private bool _isPlaying = true;
 
+    [Header("Hearts UI")]
+    public Image[] heartIcons;
+
+    [Header("Distance UI")]
+    public Text distanceText;
+
+    [Header("Game Over")]
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+
+    private void Start()
+    {
+        UpdateHeartsUI();
+        gameOverPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        distanceText.text = "Distance traveled: " + Mathf.Max(0, Mathf.FloorToInt(transform.position.x)) + "m";
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage();
+        }
+    }
+
     public void TakeDamage(int damage = 1)
     {
-        if (!_isPlaying) { return; }
+        if (!_isPlaying) return;
 
         healths -= damage;
         Debug.Log("Zycia: " + healths);
+        UpdateHeartsUI();
 
         if (healths <= 0)
         {
@@ -22,6 +54,22 @@ public class PlayerHealth : MonoBehaviour
     private void Die()
     {
         _isPlaying = false;
-        // Game over screen
+        gameOverText.text = "   Distance: " + Mathf.FloorToInt(transform.position.x);
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void UpdateHeartsUI()
+    {
+        for (int i = 0; i < heartIcons.Length; i++)
+        {
+            heartIcons[i].enabled = i < healths;
+        }
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
